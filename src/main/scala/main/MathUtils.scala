0b47ba9df715249c4f3fa8ad35324c27c2e1dd1b
martin <martoo6@gmail.com>
2015-10-13 01:10:48 -0300

    Cleanup. Added color palettes. Added math functions.

--- src/main/scala/main/MathUtils.scala
+++ src/main/scala/main/MathUtils.scala
@@ -9,4 +9,23 @@ trait MathUtils {
   def random(ceil: Double) = Math.random * ceil
 
   def lerp(v1:Double,v2:Double,amount:Double) = (v1+v2)/amount
+
+  //Build LMap using typeclass for mapping efficiently over a list
+
+  def map(value:Double, in_min:Double, in_max: Double, out_min:Double, out_max: Double) = {
+
+//    val in_range = (in_max-in_min)
+    //    val out_range = (out_max-out_min)
+    //    val a = (value-in_min)/in_range
+    //    a*out_range + out_min
+    ((value-in_min)/(in_max-in_min))*(out_max-out_min) + out_min
+  }
+
+  def constrain(value:Double, min:Double, max:Double) = {
+    //Faster than match ???
+    if(value<min) min
+    else if(value>max) max
+    else value
+  }
+
 }
--- src/main/scala/main/ThreeJSApp.scala
+++ src/main/scala/main/ThreeJSApp.scala
@@ -3,14 +3,19 @@ package main
 
 import scala.scalajs.js._
 
+//Some IDEs will autmatically delete import java.lang.Math._   , try to keep it
+//########################
+import java.lang.Math._
+//########################
+
 object ThreeJSApp extends JSApp with BasicCanvas with Helpers with PerlinNoise{
   var controls:OrbitControls = null
 
   def main():Unit = {
     Setup.Center
 
-    addAmbientLight(0x404040)
-    addDirectionalLight(0x40FF40, 0.9, (0,1,0))
+    addAmbientLight(0xFFFFFF)
+    addDirectionalLight(0xFFFFFF, 0.9, (0,1,0))
 
     controls = addOrbitControls(origin)
 
@@ -19,7 +24,7 @@ object ThreeJSApp extends JSApp with BasicCanvas with Helpers with PerlinNoise{
   }
 
   def render():Unit = {
-    stroke(Palette.pop.getRandom)
+    stroke(Palette.iDemandPancake.getRandom)
 
     val fc = frameCount*0.02
     val x = noise(fc,0,0)*500
@@ -29,8 +34,7 @@ object ThreeJSApp extends JSApp with BasicCanvas with Helpers with PerlinNoise{
     val dest = (x,y,z)
     line(origin,dest)
 
-
-    fillLambert(Palette.blue.getRandom)
+    fillLambert(Palette.iDemandPancake.getRandom)
 
     val size = random(2,5)
 
